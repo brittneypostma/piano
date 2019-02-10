838b1e27fa4cf640bab0bfb21b6651eb5b6885c5
sballgirl11 <45889730+sballgirl11@users.noreply.github.com>
2019-02-09 21:23:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var $form = $('form#test-form'),
-url = 'https://script.google.com/macros/s/AKfycbzSZsptIhfRJcDlNEQVUnUvpUPQejdM1hKmmhI3x7CeFXkPAog/exec'
 
-$('#submit-form').on('click', function(e) {
-e.preventDefault();
-var jqxhr = $.ajax({
-url: url,
-method: "GET",
-dataType: "json",
-data: $form.serializeObject()
-}).success(
-// do something
-);
-})
